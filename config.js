8933bd9ad3d99bce3ac81b14a1b630bd1509a19d
DINUWH-MD <dinethnethruvan772@gmail.com>
2024-12-30 19:12:25 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,9 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
-ALIVE_IMG: process.env.ALIVE_IMG, || "https://i.ibb.co/mcWTCp2/3586.jpg"
+SESSION_ID: process.env.SESSION_ID ||"",
+MONGODB: process.env.MONGODB || "",
+ALIVE_IMG: process.env.ALIVE_IMG, || "https://i.ibb.co/mcWTCp2/3586.jpg",
 ALIVE_MSG: process.env.ALIVE_IMG, || "*හායි ලස්සන ළමයෝව් DINUWH-MD බොට් වෙතව සාදරෙයෙන් පිලිගන්නේ නෑ🥲*"
 };
+
